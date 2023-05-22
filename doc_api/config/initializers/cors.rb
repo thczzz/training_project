93b5bef3c05e5fc1285268be04780427255b5289
echonykrom <echonykrom@UbuntuVM.myguest.virtualbox.org>
2023-05-22 11:33:22 +0300

    cors fix

--- doc_api/config/initializers/cors.rb
+++ doc_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001" # Development
+    origins "#{request.host}:3001" # Development
 
     resource "*",
       headers: :any,
